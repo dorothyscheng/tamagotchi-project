d9d438fe799dbbe26780fff212fa239cac0b9339
dcheng <cheng.dorothy.s@gmail.com>
2021-04-19 18:40:36 -0600

    Added Tamagotchi creation, start form click events, and update game stats function

--- app.js
+++ app.js
@@ -5,10 +5,63 @@ class Tamagotchi {
         this.hunger=0;
         this.sleepiness=0;
         this.boredom=0;
-        Tamagotchi.player.push(this);
+        Tamagotchi.player=this;
     }
     static player={};
 }
 
+function namePet(name) {
+    new Tamagotchi(name);
+    startGame();
+}
+
+function startGame() {
+    const player=Tamagotchi.player;
+    $('#start-screen').fadeOut();
+    $('#title').fadeOut();
+    $('#title').hide().text(player.name).fadeIn();
+    $('#play-screen').fadeIn();
+};
+
+function updateStats() {
+    const $hungryBars=$('#hungry-bar-head').children();
+    const $sleepyBars=$('#sleepy-bar-head').children();
+    const $boredBars=$('#bored-bar-head').children();
+    const player=Tamagotchi.player;
+    for (let i=0; i<$hungryBars.length; i++) {
+        const $currentBar=$hungryBars.eq(i);
+        const index=$currentBar.attr('id').charAt(6);
+        if (player.hunger>index) {
+            $currentBar.addClass('filled');
+        } else {
+            $currentBar.removeClass('filled');
+        };
+    };
+    for (let i=0; i<$sleepyBars.length; i++) {
+        const $currentBar=$sleepyBars.eq(i);
+        const index=$currentBar.attr('id').charAt(6);
+        if (player.sleepiness>index) {
+            $currentBar.addClass('filled');
+        } else {
+            $currentBar.removeClass('filled');
+        };
+    };
+    for (let i=0; i<$boredBars.length; i++) {
+        const $currentBar=$boredBars.eq(i);
+        const index=$currentBar.attr('id').charAt(5);
+        if (player.boredom>index) {
+            $currentBar.addClass('filled');
+        } else {
+            $currentBar.removeClass('filled');
+        };
+    };
+}
+
 // EVENT LISTENERS
 $('#pet-name').on('click',()=>$('#pet-name').val(''));
+$('#submit-button').on('click',(e)=>{
+    e.preventDefault();
+    if ($('#pet-name').val().trim()) {
+        namePet($('#pet-name').val().trim());
+    };
+});
